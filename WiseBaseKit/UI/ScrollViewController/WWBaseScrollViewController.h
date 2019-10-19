8314e28d1d8143b81881aeb3c3061a4d3f52f37a
wise <99487616@qq.com>
2019-10-19 10:47:05 +0800

    scrollViewcontroller 支持下拉刷新

--- WiseBaseKit/UI/ScrollViewController/WWBaseScrollViewController.h
+++ WiseBaseKit/UI/ScrollViewController/WWBaseScrollViewController.h
@@ -18,6 +18,42 @@ NS_ASSUME_NONNULL_BEGIN
 // 内容view
 @property (nonatomic, strong) UIView *contentView;
 
+//是否允许下拉刷新，默认为false
+@property(nonatomic, assign) BOOL showRefreshHeader;
+
+//是否允许上拉加载，默认为false
+@property(nonatomic, assign) BOOL showRefreshFooter;
+
+/**
+ *  开始下拉刷新
+ */
+- (void)startRefreshHeader;
+
+/**
+ *  开始上拉刷新
+ */
+- (void)startRefreshFooter;
+
+/**
+ *  停止下拉刷新
+ */
+- (void)stopRefreshHeader;
+
+/**
+ *  停止上拉刷新
+ */
+- (void)stopRefreshFooter;
+
+/**
+ *  正在下拉刷新，子类可重写实现
+ */
+- (void)refreshingHeader;
+
+/**
+ *  正在上拉刷新，子类可重写实现
+ */
+- (void)refreshingFooter;
+
 @end
 
 NS_ASSUME_NONNULL_END
--- WiseBaseKit/UI/ScrollViewController/WWBaseScrollViewController.m
+++ WiseBaseKit/UI/ScrollViewController/WWBaseScrollViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "WWBaseScrollViewController.h"
+#import <MJRefresh/MJRefresh.h>
 
 @interface WWBaseScrollViewController ()
 
@@ -17,6 +18,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    _showRefreshHeader = false;
+    _showRefreshFooter = false;
     
     _scrollView = [UIScrollView new];
     _scrollView.backgroundColor = [UIColor clearColor];
@@ -30,9 +34,6 @@
     //隐藏垂直条
     _scrollView.showsVerticalScrollIndicator = false;
     
-    //禁止回弹
-    _scrollView.bounces = false;
-    
     _contentView = [UIView new];
     [_scrollView addSubview:_contentView];
     
@@ -42,5 +43,96 @@
     }];
 }
 
+#pragma mark - setter
+
+- (void)setShowRefreshHeader:(BOOL)showRefreshHeader
+{
+    _showRefreshHeader = showRefreshHeader;
+    
+    @weakify(self)
+    
+    if (_showRefreshHeader) {
+        _scrollView.mj_header = [MJRefreshNormalHeader headerWithRefreshingBlock:^{
+            @strongify(self)
+            [self refreshingHeader];
+        }];
+    }
+    else {
+        _scrollView.mj_header = nil;
+    }
+}
+
+- (void)setShowRefreshFooter:(BOOL)showRefreshFooter
+{
+    _showRefreshFooter = showRefreshFooter;
+    
+    @weakify(self)
+    
+    if (_showRefreshFooter) {
+        _scrollView.mj_footer = [MJRefreshBackNormalFooter footerWithRefreshingBlock:^{
+            @strongify(self)
+            [self refreshingFooter];
+        }];
+    }
+    else {
+        _scrollView.mj_footer = nil;
+    }
+}
+
+/**
+ *  开始下拉刷新
+ */
+- (void)startRefreshHeader
+{
+    [_scrollView.mj_header beginRefreshing];
+}
+
+/**
+ *  开始上拉刷新
+ */
+- (void)startRefreshFooter
+{
+    [_scrollView.mj_footer beginRefreshing];
+}
+
+/**
+ *  停止下拉刷新
+ */
+- (void)stopRefreshHeader
+{
+    [_scrollView.mj_header endRefreshing];
+}
+
+/**
+ *  停止上拉刷新
+ */
+- (void)stopRefreshFooter
+{
+    [_scrollView.mj_footer endRefreshing];
+}
+
+
+/**
+ *  正在下拉刷新，子类可重写实现
+ */
+- (void)refreshingHeader
+{
+    [self stopRefreshHeader];
+}
+
+/**
+ *  正在上拉刷新，子类可重写实现
+ */
+- (void)refreshingFooter
+{
+    [self stopRefreshFooter];
+}
+
+- (void)setShowType:(WWBaseKitShowType)showType
+{
+    [super setShowType:showType];
+    
+    _scrollView.hidden = !(showType == WWBaseKitShowType_Normal);
+}
 
 @end
